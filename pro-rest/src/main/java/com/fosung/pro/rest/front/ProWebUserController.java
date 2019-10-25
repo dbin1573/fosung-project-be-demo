6c5dae2e653ea995b4aeaa131833fba003091281
dbin1573 <34564624+dbin1573@users.noreply.github.com>
2019-10-25 14:17:53 +0800

    首页修改了使用了fetch异步请求（没有使用xhr）将数据信息遍历出来

--- pro-rest/src/main/java/com/fosung/pro/rest/front/ProWebUserController.java
+++ pro-rest/src/main/java/com/fosung/pro/rest/front/ProWebUserController.java
@@ -4,9 +4,18 @@ import com.fosung.pro.entity.User;
 import com.fosung.pro.service.IUserService;
 import com.google.common.collect.Maps;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.View;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
@@ -16,22 +25,23 @@ import java.util.Map;
 public class ProWebUserController {
 
     @Autowired
-    private IUserService iUserService;
-
+    IUserService iUserService;
+    @Autowired
+    StringRedisTemplate srt;
     @RequestMapping("/add")
-    public String add() {
-        User user = new User();
+    public String add(User user) {
+       /* User user = new User();
         user.setUserName("a");
         user.setPassWord("b");
-        user.setRealName("c");
+        user.setRealName("c");*/
 
         iUserService.save(user);
         return "add....";
     }
 
     @RequestMapping("/del")
-    public String del() {
-        iUserService.delete(3375252250560565192L);
+    public String del(Long id) {
+        iUserService.delete(id);//3375252250560565192L
         return "del....";
     }
 
@@ -47,15 +57,27 @@ public class ProWebUserController {
     }
 
     @RequestMapping("/list")
-    public String sea() {
+    public List<User> sea() {
         Map<String, Object> map = Maps.newHashMap();
 
         map.put("name", "a");
 
         List<User> ls = iUserService.queryAll(map);
-        return "sea...." + ls.toString() + ls.size();
+        return  ls;
     }
 
+    @RequestMapping("/")
+    public void fastView(HttpServletResponse resp) throws IOException {
+      /*  Cookie cname=new Cookie("name","张洪彬");
+        cname.setMaxAge(60*60);
+        resp.addCookie(cname);*/
+
+
+        srt.opsForValue().set("name","张洪彬");
+        String s=srt.opsForValue().get("name");
+        System.out.println(s);
+        resp.sendRedirect("http://localhost:63342/main/pro-rest/templates/vuser.html");
+    }
 
 }
 
